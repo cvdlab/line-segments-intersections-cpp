23e2dfc59129b4e7d7ed7c9f73dc95dbeb314e1f
Danilo Salvati <sadan91@gmail.com>
2016-05-24 12:32:47 +0200

    Fixed determinant computation function

--- src/Utils/LineUtils.cpp
+++ src/Utils/LineUtils.cpp
@@ -6,7 +6,7 @@
 #include "../../headers/Utils/LineUtils.h"
 
 /* Find the intersection point between two lines */
-Point *Inters::LineUtils::findIntersection(Line *l1, Line *l2) {
+Inters::Point *Inters::LineUtils::findIntersection(Line *l1, Line *l2) {
 
     /* First line goes between a and b */
     Point *a = l1->getV0();
@@ -30,7 +30,7 @@ Point *Inters::LineUtils::findIntersection(Line *l1, Line *l2) {
 
     double mu = determinant(a->getX() - b->getX(), d->getX() - b->getX(), a->getY() - b->getY(),
                             d->getY() - b->getY()) / det;
-
+    
     if (lambda >= 0 && lambda <= 1 && mu >= 0 && mu <= 1) {
         /* Compute intersection point */
         double intersX = lambda * a->getX() + (1 - lambda) * b->getX();
@@ -45,12 +45,12 @@ Point *Inters::LineUtils::findIntersection(Line *l1, Line *l2) {
 
 /** Find the determinant of a 2x2 matrix **/
 double Inters::LineUtils::determinant(double m1, double m2, double m3, double m4) {
-    return m1 * m2 - m3 * m4;
+    return m1 * m4 - (m2 * m3);
 }
 
 
 /** Find the intersection between a segment and a vertical line **/
-Point *Inters::LineUtils::findVerticalIntersection(Line l1, double verticalLineX) {
+Inters::Point *Inters::LineUtils::findVerticalIntersection(Line l1, double verticalLineX) {
     double deltaY = l1.getV1()->getY() - l1.getV0()->getY();
     double deltaX = l1.getV1()->getX() - l1.getV0()->getX();
     return new Point(verticalLineX, deltaY / deltaX * (verticalLineX - l1.getV0()->getX()) + l1.getV0()->getY());
